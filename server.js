926f9e02acfd94771827c3cf01a1ce77ab067493
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-09-21 18:51:33 +0100

    setup backend

---
+++ server.js
@@ -0,0 +1,27 @@
+const PORT = 8000;
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/flights', (req, res) => {
+        const options = {
+            url: `${process.env.URL}?page-size=20`,
+            headers: {
+                accept: 'application/json',
+                'X-Cassandra-Token': process.env.X_CASSANDRA_TOKEN
+            }
+            
+        };
+
+        axios.request(options).then(response => {
+            console.log(response.data);
+            res.json(response.data);
+        }).catch(error => {
+            console.log(error);
+        });
+    });
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
